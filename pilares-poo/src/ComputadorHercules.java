3a71cf5c030bfd3293007a3c0e11aba484cdce37
Hercules Herculano <hbheroi@gmail.com>
2024-09-18 00:24:23 -0300

    exercitando encapsulamento

---
+++ pilares-poo/src/ComputadorHercules.java
@@ -0,0 +1,13 @@
+public class ComputadorHercules {
+    public static void main(String[] args) {
+        
+        //abrindo MSN Messenger
+        MSNMessenger msn = new MSNMessenger();
+
+        
+        msn.enviarMensagem();
+        
+
+        msn.receberMensagem();
+    }
+}
